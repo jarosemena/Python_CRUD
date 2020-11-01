10a0584fb76f287413509370e0fe7d21cde87dc1
Jose Jair <jjarosemena@gmail.com>
2020-10-31 23:34:34 -0500

    adelantos del proyecto por listas

--- main.py
+++ main.py
@@ -3,25 +3,25 @@ import sys
 #clients = ['pablo', 'ricardo']
 
 clients = [
-        { 
+        {
             'name' :'Pablo',
-            'company' : 'paythonorg',  
+            'company' : 'paythonorg',
             'email' : 'pablo@google.com',
             'psition' : 'Software engineer'
         },
         {
             'name' :'Ricardo',
-            'company' : 'Facebook',  
+            'company' : 'Facebook',
             'email' : 'Ricardo@facebook.com',
             'psition' : 'Data engineer'
         }
-        ]
+    ]
 
 
 def create_client(client):
     global clients
     if client not in clients:
-       clients.append(client) 
+       clients.append(client)
     else:
         print('Client already is in the client\'s list')
 
@@ -32,7 +32,7 @@ def find_client(client_name):
     for client in clients:
         if client['name'] == client_name:
             result = 'the client is in the list of clients'
-    
+
     return result
 
 def list_clients():
@@ -72,79 +72,78 @@ def _print_welcome():
     print('[D]elete client')
     print('[U]pdate client')
     print('[F]ind client')
-    
+
 
 def _get_client_name():
-   client_name= None
+    client_name= None
 
-   while not client_name:
-       client_name = input('what is the client name? ')
+    while not client_name:
+        client_name = input('what is the client name? ')
 
-       if client_name == 'exit':
-           client_name= None
-           break
+        if client_name == 'exit':
+            client_name= None
+            break
 
-   if not client_name:
+    if not client_name:
         sys.exit()
 
-   return client_name
+    return client_name
 
 
 def _get_client():
-   client_name= None
-   company = None
-   email = None
-   position = None
-   client = {}
+    client_name= None
+    company = None
+    email = None
+    position = None
+    client = {}
 
-   while not client_name:
-       client_name = input('what is the client name? ')
+    while not client_name:
+        client_name = input('what is the client name? ')
 
-       if client_name == 'exit':
-           client_name= None
-           break
+        if client_name == 'exit':
+            client_name= None
+            break
 
-   if not client_name:
+    if not client_name:
         sys.exit()
 
 
-   while not company:
-       company = input('what is the client\'s company name? ')
+    while not company:
+        company = input('what is the client\'s company name? ')
 
-       if company == 'exit':
-           company= None
-           break
+        if company == 'exit':
+            company= None
+            break
 
-   if not company:
-        sys.exit()
-   
-
-   while not email:
-       email = input('what is the client\'s Email? ')
-
-       if email == 'exit':
-           email = None
-           break
-
-   if not email:
+    if not company:
         sys.exit()
 
+    while not email:
+        email = input('what is the client\'s Email? ')
 
-   while not position:
-       position = input('what is the client\'s position? ')
+        if email == 'exit':
+            email = None
+            break
+
+    if not email:
+        sys.exit()
+
+
+    while not position:
+        position = input('what is the client\'s position? ')
 
        if position == 'exit':
-           position = None
-           break
+        position = None
+        break
 
-   if not position:
+    if not position:
         sys.exit()
-   
-   client = {
-           'name' : client_name,
-           'company' : company,
-           'email' : email, 
-           'position' : position
+
+    client = {
+        'name' : client_name,
+        'company' : company,
+        'email' : email,
+        'position' : position
            }
 
    return client
@@ -159,26 +158,26 @@ if  __name__ == '__main__':
     if command == 'EXIT':
         sys.exit()
     else:
-   	if command == 'C':
-   	    client = _get_client() 
-   	    list_clients()
-   	    create_client(client)
-   	    list_clients()
-   	elif command =='D':
-   	    client_name = _get_client_name() 
-   	    list_clients()
-   	    delete_client(client_name)
-   	    list_clients()
-   	elif command =='U':
-   	    client_name = _get_client_name()
-   	    updated_client = _get_client()
-   	    update_client(client_name,updated_client)
-   	    list_clients()
-   	elif command == 'F':
-   	    client_name = _get_client_name()
-   	    print(find_client(client_name))
+    if command == 'C':
+        client = _get_client()
+        list_clients()
+        create_client(client)
+        list_clients()
+    elif command =='D':
+        client_name = _get_client_name(i)
+        list_clients()
+        delete_client(client_name)
+        list_clients()
+    elif command =='U':
+        client_name = _get_client_name()
+        updated_client = _get_client()
+        update_client(client_name,updated_client)
+        list_clients()
+    elif command == 'F':
+        client_name = _get_client_name()
+        print(find_client(client_name))
 #  	     list_clients()
-   	else: 
-   	    print('invalid command')
+    else:
+        print('invalid command')
 
 
